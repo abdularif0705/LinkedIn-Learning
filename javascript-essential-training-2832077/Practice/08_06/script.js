5020a1faa3f93f359d2d778292493c614b86efb0
Abdul Arif <abdul.arif0705@gmail.com>
2024-06-06 23:36:58 -0400

    targeting DOM objects not string

--- javascript-essential-training-2832077/Practice/08_06/script.js
+++ javascript-essential-training-2832077/Practice/08_06/script.js
@@ -12,17 +12,17 @@ function createArticle(data) { // This function will create an article element w
     const newArticle = document.createElement("article")
     newArticle.classList.add("newClass")
     newArticle.innerHTML = `
-        <h2>${data.title}</h2>
+        <h1>${data.title}</h1>
         <p>${data.body}</p>
     `
     return newArticle
 }
 
-const changeColor = (element, color) => {
+const changeBackgroundColor = (element, color) => {
     element.style.backgroundColor = color
 }
 
-const changeColor2 = function (element, color) {
+const changeColor = function (element, color) {
     element.style.color = color
 }
 
@@ -31,7 +31,15 @@ const data = {
     body: "This is a test article",
 }
 
-changeColor(data.body, "pink") // This will change the background color of the body of the data object to pink
-changeColor2(data.body, "rebeccapurple") // This will change the color of the body of the data object to rebeccapurple
-console.log(data) // This will log the body of the data object to the console
-main.append(createArticle(data)) // This will append the article to the main element
+const article = createArticle(data); // Create the article element
+main.append(article); // Append the article to the main element
+
+const articleBody = article.querySelector("p"); // Select the paragraph element inside the article
+changeBackgroundColor(articleBody, "pink"); // Change the background color of the paragraph to pink
+changeColor(articleBody, "rebeccapurple"); // Change the text color of the paragraph to rebeccapurple
+
+const articleTitle = article.querySelector("h1"); // Select the h1 element inside the article
+changeBackgroundColor(articleTitle, "lightblue"); // Change the background color of the paragraph to pink
+changeColor(articleTitle, "royalblue"); // Change the text color of the paragraph to rebeccapurple
+
+console.log(data); // This will log the data object to the console
