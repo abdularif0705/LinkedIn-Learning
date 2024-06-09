16f5ee6d35a3e84e6c98efdae290e98314eff8f5
Abdul Arif <abdul.arif0705@gmail.com>
2024-06-09 00:28:46 -0400

    using useState vs useReducer

--- Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/finished/react-app/src/App.js
+++ Ex_Files_React_js_EssT/Exercise Files/Ch04/04_07/finished/react-app/src/App.js
@@ -1,21 +1,18 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useReducer, useState } from "react";
 
 function App() {
-  const [checked, setChecked] = useReducer(
-    (checked) => !checked,
-    false
-  );
+  const [checked, setChecked] = useState(false);
   return (
     <div className="App">
-      <input
-        type="checkbox"
-        value={checked}
-        onChange={setChecked}
-      />
-      <label>
-        {checked ? "checked" : "not checked"}
-      </label>
+      <input 
+      type="checkbox" 
+      value={checked} 
+      onChange={() => 
+        setChecked((checked) => !checked)
+      } /> {/* the value of the checkbox is set to the value of the checked state variable. When the checkbox is clicked, the onChange event handler is triggered and the setChecked function is called with a callback function that toggles the value of the checked state variable */}
+      <label>{checked ? "Checked" : "Not Checked"}</label>
+      
     </div>
   );
 }
